75959e1a999617745d09171bc63ebd3dfba5228b
1022_IlI_Sarma.M <sarmamurugan07@gmail.com>
2024-12-02 22:21:43 +0200

    add the

--- index.js
+++ index.js
@@ -1,3 +1,43 @@
+// import jsonfile from "jsonfile";
+// import moment from "moment";
+// import simpleGit from "simple-git";
+// import random from "random";
+
+// const path = "./data.json";
+
+// const markCommit = (x, y) => {
+//   const date = moment()
+//     .subtract(1, "y")
+//     .add(1, "d")
+//     .add(x, "w")
+//     .add(y, "d")
+//     .format();
+
+//   const data = {
+//     date: date,
+//   };
+
+//   jsonfile.writeFile(path, data, () => {
+//     simpleGit().add([path]).commit(date, { "--date": date }).push();
+//   });
+// };
+
+// const makeCommits = (n) => {
+//   if(n===0) return simpleGit().push();
+//   const x = random.int(0, 54);
+//   const y = random.int(0, 6);
+//   const date = moment().subtract(1, "y").add(1, "d").add(x, "w").add(y, "d").format();
+
+//   const data = {
+//     date: date,
+//   };
+//   console.log(date);
+//   jsonfile.writeFile(path, data, () => {
+//     simpleGit().add([path]).commit(date, { "--date": date },makeCommits.bind(this,--n));
+//   });
+// };
+
+// makeCommits(100);
 import jsonfile from "jsonfile";
 import moment from "moment";
 import simpleGit from "simple-git";
@@ -5,6 +45,17 @@ import random from "random";
 
 const path = "./data.json";
 
+// Helper function to get a random date in 2023
+const getRandomDateIn2023 = () => {
+  // Generate a random date in 2023 between Jan 1 and Dec 31
+  const startOfYear = moment('2023-01-01');
+  const endOfYear = moment('2023-12-31');
+
+  const randomDate = moment(startOfYear).add(random.int(0, endOfYear.diff(startOfYear, 'days')), 'days');
+  return randomDate.format();
+};
+
+// Commit function with a specific date
 const markCommit = (x, y) => {
   const date = moment()
     .subtract(1, "y")
@@ -22,19 +73,23 @@ const markCommit = (x, y) => {
   });
 };
 
+// Function to make multiple commits
 const makeCommits = (n) => {
-  if(n===0) return simpleGit().push();
-  const x = random.int(0, 54);
-  const y = random.int(0, 6);
-  const date = moment().subtract(1, "y").add(1, "d").add(x, "w").add(y, "d").format();
+  if (n === 0) return simpleGit().push();
+
+  const date = getRandomDateIn2023(); // Get a random date in 2023
 
   const data = {
     date: date,
   };
-  console.log(date);
+  console.log(date); // Optionally log the date
+
   jsonfile.writeFile(path, data, () => {
-    simpleGit().add([path]).commit(date, { "--date": date },makeCommits.bind(this,--n));
+    simpleGit()
+      .add([path])
+      .commit(date, { "--date": date }, () => makeCommits(n - 1)); // Decrease n
   });
 };
 
+// Make 100 commits
 makeCommits(100);
